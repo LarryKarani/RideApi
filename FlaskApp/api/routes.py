5dc71f61d02983e6faeb954d98b97e7bda187609
Larry karani <karanilarry@gmail.com>
2018-07-01 08:26:31 +0300

    removed unused jwt imports

--- FlaskApp/api/routes.py
+++ FlaskApp/api/routes.py
@@ -1,4 +1,4 @@
-from flask_jwt_extended import(create_access_token, create_refresh_token, jwt_required,jwt_refresh_token_required,get_jwt_identity,get_raw_jwt)
+from flask_jwt_extended import(create_access_token, create_refresh_token, jwt_required,jwt_refresh_token_required,get_jwt_identity)
 from flask_restful import Resource, reqparse
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import request, jsonify, redirect
