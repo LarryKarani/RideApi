3595ade7d988d0e812d528be4ad20eaf145b807a
Larry karani <karanilarry@gmail.com>
2018-07-04 23:02:37 +0300

    updated requirements.txt updated travis yml file edit procfile

--- FlaskApp/tests/test_routes.py
+++ FlaskApp/tests/test_routes.py
@@ -16,7 +16,7 @@ class RegisterTestCase(unittest.TestCase):
     def setUp(self):
        self.dummy_client = app.test_client()
        self.ride_offer = dummy_ride_offer
-       self.request_j = dummy_ride_offer_request
+       self.join_request = dummy_ride_offer_request
        self.incomplete_offer = dummy_incomplete_offer
        self.test_user = dummy_user
        self.test_login = dummy_login
@@ -102,7 +102,7 @@ class RegisterTestCase(unittest.TestCase):
         self.response = self.dummy_client.post('/api/v1/auth/login', data=json.dumps(self.test_login), headers={"content-type":"application/json"})
         self.token  = json.loads(self.response.data)['acces_token']
         self.response = self.dummy_client.post('/api/v1/rides', data= json.dumps(self.ride_offer), headers={'content-type':'application/json', 'Authorization':'Bearer {}'.format(self.token)}) 
-        self.response = self.dummy_client.post('/api/v1/rides/1/requests', data=json.dumps(self.request_j), headers={'content-type':'application/json'})
+        self.response = self.dummy_client.post('/api/v1/rides/1/requests', data=json.dumps(self.join_request), headers={'content-type':'application/json'})
         self.assertEqual(self.response.status_code, 401)
 
     def test_request_to_join_with_incomplete_details_with_tokens(self):
@@ -112,7 +112,3 @@ class RegisterTestCase(unittest.TestCase):
         self.response = self.dummy_client.post('/api/v1/rides', data= json.dumps(self.ride_offer), headers={'content-type':'application/json', 'Authorization':'Bearer {}'.format(self.token)}) 
         self.response = self.dummy_client.post('/api/v1/rides/1/requests', data=json.dumps(self.incomplete_request), headers={'content-type':'application/json', 'Authorization':'Bearer {}'.format(self.token)})
         self.assertEqual(self.response.status_code, 400)
-
-    
-
-
